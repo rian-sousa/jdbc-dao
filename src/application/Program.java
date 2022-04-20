92248a300e356f7284ded2ad0db6184b6bd3c240
Rian Sousa <99686158+rian-sousa@users.noreply.github.com>
2022-04-20 17:10:06 -0300

    findById implementation

--- src/application/Program.java
+++ src/application/Program.java
@@ -11,13 +11,12 @@ public class Program {
 
     public static void main(String[] args){
 
-        Department obj = new Department(1, "Books");
-        Seller seller = new Seller(21, "Bob", "Bob@gmail.com", new Date(), 3000.0, obj);
-
         SellerDao sellerDao = DaoFactory.createSellerDao();
 
+        Seller seller = sellerDao.findById(3);
+
         System.out.println(seller);
-        System.out.println(obj);
+        //System.out.println(obj);
 
     }
     
--- src/db/DB.java
+++ src/db/DB.java
@@ -4,6 +4,7 @@ import java.io.FileInputStream;
 import java.io.IOException;
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
 import java.util.Properties;
@@ -58,4 +59,15 @@ public class DB {
             }
         }
     }
+
+    public static void closeResultSet(ResultSet rs){
+        if(rs != null){
+            try{
+                rs.close();
+            } catch (SQLException e){
+                throw new DbException(e.getMessage());
+            }
+        }
+    }
+
 }
